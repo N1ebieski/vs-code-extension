43fcb10d8e58e1156d9e7e323febf948656332e8
N1ebieski <kontakt@intelekt.net.pl>
2025-03-11 09:16:26 +0000

    my version of php-parser

--- src/support/parser.ts
+++ src/support/parser.ts
@@ -36,13 +36,13 @@ export const setParserBinaryPath = (context: vscode.ExtensionContext) => {
 };
 
 const downloadBinary = async (context: vscode.ExtensionContext) => {
-    const binaryVersion = "0.1.42";
+    const binaryVersion = "0.1.42+intelekt.1";
     const osPlatform = os.platform();
     const osArch = os.arch();
     const extension = osPlatform === "win32" ? ".exe" : "";
     const filename = `php-parser-v${binaryVersion}-${osArch}-${osPlatform}${extension}`;
 
-    const uri = `https://github.com/laravel/vs-code-php-parser-cli/releases/download/v${binaryVersion}/${filename}`;
+    const uri = `https://github.com/n1ebieski/vs-code-php-parser-cli/releases/download/v${binaryVersion}/${filename}`;
 
     const logger = new OutputLogger(`File Downloader`, context);
     const requestHandler = new HttpRequestHandler(logger);
