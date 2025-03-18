7c08b90a56c6ceee8beba6c91cd24230ce971cb8
N1ebieski <kontakt@intelekt.net.pl>
2025-03-18 20:42:04 +0000

    php-parser version

--- src/support/parser.ts
+++ src/support/parser.ts
@@ -36,7 +36,7 @@ export const setParserBinaryPath = (context: vscode.ExtensionContext) => {
 };
 
 const downloadBinary = async (context: vscode.ExtensionContext) => {
-    const binaryVersion = "0.1.42+intelekt.3";
+    const binaryVersion = "0.1.42+intelekt.4";
     const osPlatform = os.platform();
     const osArch = os.arch();
     const extension = osPlatform === "win32" ? ".exe" : "";
